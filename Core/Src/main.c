d47f0d7baf5dfcbe387dd39b579443561dceb162
CoderLiuChina <tiedan908@163.com>
2024-08-12 00:22:00 +0800

    删除无用注释

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -61,15 +61,15 @@ void PS2toUSB();
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 extern USBD_HandleTypeDef hUsbDeviceFS;
-//uint8_t i = 0;
+// uint8_t i = 0;
 uint8_t USB_Upload_data[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -110,32 +110,14 @@ int main(void)
     PS2_GetData(&PS2);
 
     // DeadZone
-    /*
-    if(abs(PS2.PSS_LX-LXCache)>=10||PS2.PSS_LX == 0)
-      LXCache = PS2.PSS_LX;
-    else
-      PS2.PSS_LX = LXCache;
-    if (abs(PS2.PSS_RX - RXCache) >= 10 || PS2.PSS_RX == 0)
-      RXCache = PS2.PSS_RX;
-    else
-      PS2.PSS_RX = RXCache;
-    if (abs(PS2.PSS_RY- RYCache) >= 10 || PS2.PSS_RY == 0)
-      RYCache = PS2.PSS_RY;
-    else
-      PS2.PSS_RY = RYCache;
-    if (abs(PS2.PSS_LY - LYCache) >= 10 || PS2.PSS_LY == 0)
-      LYCache = PS2.PSS_LY;
-    else
-      PS2.PSS_LY = LYCache;
-    */
-    if(abs(PS2.PSS_LX-127)<abs(PS2.PSS_LY-127))
+    if (abs(PS2.PSS_LX - 127) < abs(PS2.PSS_LY - 127))
     {
-      if(abs(PS2.PSS_LX)<=abs(PS2.PSS_LY-127)/8)
+      if (abs(PS2.PSS_LX) <= abs(PS2.PSS_LY - 127) / 8)
         PS2.PSS_LX = 0;
     }
     else
     {
-      if(abs(PS2.PSS_LY)<=abs(PS2.PSS_LX-127)/8)
+      if (abs(PS2.PSS_LY) <= abs(PS2.PSS_LX - 127) / 8)
         PS2.PSS_LY = 0;
     }
 
@@ -158,9 +140,9 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -168,8 +150,8 @@ void SystemClock_Config(void)
   RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
@@ -183,9 +165,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
@@ -216,9 +197,9 @@ void PS2toUSB()
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -230,14 +211,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
